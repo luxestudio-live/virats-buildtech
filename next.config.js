69ba2f1f54f0848a35d2ae8c9e8f4767954c0baa
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2026-01-03 20:50:31 +0530

    Add GitHub Pages deployment workflow and update configuration files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const isGithubPages = process.env.NEXT_PUBLIC_BASE_PATH;
+const repoName = 'virats-buildtech';
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+    domains: ['images.unsplash.com'],
+  },
+};
 module.exports = {
   images: {
     domains: ['images.unsplash.com'],
